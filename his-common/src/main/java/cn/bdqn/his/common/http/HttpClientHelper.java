a386edc4e09ec6dee145e90378639c25d1bc6e05
as2631703329 <2631703329@qq.com>
2020-04-13 13:26:21 +0800

    诊所项目

--- his-common/src/main/java/cn/bdqn/his/common/http/HttpClientHelper.java
+++ his-common/src/main/java/cn/bdqn/his/common/http/HttpClientHelper.java
@@ -43,15 +43,15 @@ public class HttpClientHelper {
 			String result = null;
 			if(httpEntity != null) {
 				result = EntityUtils.toString(httpEntity);
-				if(log.isDebugEnabled()) {
+				/*if(log.isDebugEnabled()) {
 					log.debug("响应内容：{}",result);
-				}
+				}*/
 				if(response.getStatusLine().getStatusCode() == 200) {
 					return objectMapper.readValue(result,Response.class);
 				}
 			}
 		} catch(Exception ex) {
-			log.error(ex.getMessage(),ex);
+			/*log.error(ex.getMessage(),ex);*/
 		} finally {
 			HttpClientUtils.closeQuietly(httpClient);
 			HttpClientUtils.closeQuietly(response);
